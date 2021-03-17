5179bae392da34314e65f9c7f1f65dafe52879ef
yuchen0816 <kevinxiao0816@icloud.com>
2021-03-17 04:30:40 -0700

    add main.cpp

--- 5_2_textLCD/main.cpp
+++ 5_2_textLCD/main.cpp
@@ -1,24 +1,22 @@
-//Display ADC input data
 #include "mbed.h"
 #include "TextLCD.h"
 
+DigitalOut led(LED1);      //LED1 = PA_5
 TextLCD lcd(D2, D3, D4, D5, D6, D7);
-AnalogIn Ain(A0);
 
 int main()
 {
-      float percentage;
-      int D;
-      while (1)
+      int x=30;
+      lcd.printf("108000133\n");
+      while(true)
       {
-            percentage = Ain * 100;
-            D = int(percentage);
-            float B = percentage - D;
-            int C = B * 1000000;
-
-            lcd.printf("%d.", D);
-            lcd.printf("%d", C);
-            ThisThread::sleep_for(250ms);
-            lcd.cls();
+            led = !led;             // toggle led
+            lcd.locate(5,1);
+            lcd.printf("%5i",x);    //counter display
+            ThisThread::sleep_for(1s);
+            if(0 < x & x <= 30)
+                x = x-1;
+            else 
+                x = 30;
       }
 }
